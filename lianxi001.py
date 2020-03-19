149f5c870c4ec7ad8e99fc34a89d28e671898475
KLH <email@example.com>
2020-03-19 19:11:31 +0800

    Initial commit

---
+++ lianxi001.py
@@ -0,0 +1,8 @@
+name='konglinghui'
+print(name)
+print(name[0])
+#print(len(name))
+#print(name[21])
+print(name[2:5])
+print(name*2)
+print(name+' is a good boy.')
---
+++ lianxi002.py
@@ -0,0 +1,7 @@
+mylist=[1,2,3,4,5]
+mytuple=(1,2,3,4,5)
+# print(type(mylist))
+# print(type(mytuple))
+print(dir(mylist))
+print('.................')
+print(dir(mytuple))
